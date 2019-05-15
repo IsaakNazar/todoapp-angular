2b5b3fc9cc671014228d472e7c29e1527e95be31
nisakov <nazarbek.isakov@zensoft.io>
2019-05-15 17:28:46 +0600

    delete config service

--- src/app/todo-list/todo-list.component.ts
+++ src/app/todo-list/todo-list.component.ts
@@ -1,9 +1,5 @@
-import {Component, OnInit, OnDestroy, ɵEMPTY_ARRAY} from '@angular/core';
+import {Component, OnInit, OnDestroy} from '@angular/core';
 import {TodoService} from '../todo.service';
-import {Subscription} from 'rxjs';
-import {ConfigService} from '../config.service';
-import {Log} from '@angular/core/testing/src/logger';
-import {log} from 'util';
 
 @Component({
   selector: 'app-todo-list',
